2d9a49c4890654bf7a00147d37b7761e236023b3
irsyadibdrrhmn <irsyadibadurrahman409@gmail.com>
2025-09-04 22:37:38 +0700

    migration

--- app/Models/articlenews.php
+++ app/Models/articlenews.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class articlenews extends Model
@@ -19,4 +20,13 @@ class articlenews extends Model
         'author_id',
         'is_featured',
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(category::class, 'category_id');
+    }
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(author::class, 'author_id');
+    }
 }
