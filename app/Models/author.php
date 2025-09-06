d90bbec2fef091608d3e748de16bc7c6b542c99c
irsyadibdrrhmn <irsyadibadurrahman409@gmail.com>
2025-09-06 15:28:28 +0700

    add article

--- app/Models/author.php
+++ app/Models/author.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class author extends Model
 {
@@ -17,6 +18,11 @@ class author extends Model
         'avatar',
         'slug',
     ];
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
         public function news(): HasMany
     {
         return $this->hasMany(articlenews::class);
