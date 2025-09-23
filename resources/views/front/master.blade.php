ea95d1973ae59c0697e565386bc3ac205d72ff8d
irsyadibdrrhmn <irsyadibadurrahman409@gmail.com>
2025-09-24 06:01:42 +0700

    title

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Jezabelle | News and Article</title>
+        <title>Jezabelle | The News You Need</title>
 
         @stack('before-styles')
 		<link href="{{asset('output.css')}}" rel="stylesheet" />
